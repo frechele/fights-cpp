a10d6f07f364c60fb55509ddcc7143aee6505eba
Junyeong Park <jyp10987@gmail.com>
2023-01-28 14:33:57 +0900

    [WIP] feat(game): implement rotation

--- Includes/fights/Game.hpp
+++ Includes/fights/Game.hpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <string>
 
+#include <fights/Action.hpp>
 #include <fights/Common.hpp>
 #include <fights/Player.hpp>
 
@@ -16,11 +17,49 @@ constexpr int EXTENDED_BOARD_SIZE = BOARD_SIZE + 2;
 
 constexpr int INITIAL_WALL_COUNT_PER_PLAYER = 10;
 
+class WallBoard final
+{
+ public:
+    bool IsHorizontalWallPlaced(int x, int y) const;
+    bool IsVerticalWallPlaced(int x, int y) const;
+
+    void PlaceHorizontalWall(int x, int y);
+    void PlaceVerticalWall(int x, int y);
+    void Rotate(int x, int y);
+
+    bool CheckReachability(Point playerPos, int target) const;
+
+    std::string ToString() const;
+
+ private:
+    static constexpr int pointToHorizontalWallIndex(int x, int y)
+    {
+        return x * (BOARD_SIZE + 1) + y;
+    }
+
+    static constexpr int pointToVerticalWallIndex(int x, int y)
+    {
+        return x + y * (BOARD_SIZE + 1);
+    }
+
+    void clearBoundary();
+
+ private:
+    static constexpr unsigned char HORIZONTAL_WALL = 0b01;
+    static constexpr unsigned char VERTICAL_WALL = 0b10;
+
+    std::array<unsigned char, (BOARD_SIZE + 1) * (BOARD_SIZE + 2)> walls_{ 0 };
+};
+
 class Game final
 {
  public:
     Game();
     Game(const Game&) = default;
+    Game(Game&&) = default;
+
+    Game& operator=(const Game&) = default;
+    Game& operator=(Game&&) = default;
 
     //! Returns the color of current player
     Player GetCurrentPlayer() const;
@@ -30,19 +69,46 @@ class Game final
     //! Returns the number of remain walls of \p player
     int GetRemainWallCount(Player player) const;
 
-    //! \note DO NOT call this method directly.
-    void SetPlayerPosition(Player player, Point newPosition);
-
     //! Returns the position of \p player
     Point GetPlayerPosition(Player player) const;
 
+    //! Returns whether there is horizontal wall at \p pos
+    bool IsHorizontalWallPlaced(Point pos) const;
+
+    //! Returns whether there is vertical wall at \p pos
+    bool IsVerticalWallPlaced(Point pos) const;
+
+    //! Returns the winner
+    Player GetWinnter() const;
+
     //! Visualize the game state
     std::string ToString() const;
+
+    //! Checks \p action is valid for \p player.
+    //! \param player if Player::NONE, use the current player color
+    bool IsValidAction(const Action& action,
+                       Player player = Player::NONE) const;
+
+    //! Play the game
+    //! \param player if Player::NONE, use the current player color
+    void Play(const Action& action, Player player = Player::NONE);
+
+ private:
+    bool isValidMove(const Actions::Move& action, Player player) const;
+    bool isValidPlaceHorizontalWall(const Actions::PlaceHorizontalWall& action,
+                                    Player player) const;
+    bool isValidPlaceVerticalWall(const Actions::PlaceVerticalWall& action,
+                                  Player player) const;
+    bool isValidRotate(const Actions::Rotate& action, Player player) const;
+
+    void setPlayerPosition(Player player, Point newPosition);
 
  private:
     std::array<Point, 2> playerPositions_;
     std::array<int, 2> remainWallCounts_{ INITIAL_WALL_COUNT_PER_PLAYER,
                                           INITIAL_WALL_COUNT_PER_PLAYER };
+
+    WallBoard wallBoard_;
 
     Player player_{ Player::BLUE };
 };
--- Sources/fights/Game.cc
+++ Sources/fights/Game.cc
@@ -1,13 +1,199 @@
 #include <fights/Game.hpp>
 
 #include <sstream>
+#include <stdexcept>
 
 namespace fights
 {
+void WallBoard::PlaceHorizontalWall(int x, int y)
+{
+    walls_[pointToHorizontalWallIndex(x, y)] |= HORIZONTAL_WALL;
+    walls_[pointToHorizontalWallIndex(x + 1, y)] |= HORIZONTAL_WALL;
+    clearBoundary();
+}
+
+void WallBoard::PlaceVerticalWall(int x, int y)
+{
+    walls_[pointToVerticalWallIndex(x, y)] |= VERTICAL_WALL;
+    walls_[pointToVerticalWallIndex(x, y + 1)] |= VERTICAL_WALL;
+    clearBoundary();
+}
+
+void WallBoard::Rotate(int x, int y)
+{
+    auto tmpWall = walls_;
+
+    for (int dx = 0; dx <= 4; ++dx)
+    {
+        for (int dy = 0; dy <= 4; ++dy)
+        {
+            const int nx = x + dx - 1;
+            const int ny = y + dy - 1;
+
+            if (dx > 0)
+                tmpWall[pointToHorizontalWallIndex(nx, ny)] &= ~HORIZONTAL_WALL;
+
+            if (dy > 0)
+                tmpWall[pointToVerticalWallIndex(nx, ny)] &= ~VERTICAL_WALL;
+        }
+    }
+
+    for (int dx = 0; dx <= 4; ++dx)
+    {
+        for (int dy = 0; dy <= 4; ++dy)
+        {
+            const int ox = x + dx - 1;
+            const int oy = y + dy - 1;
+
+            if (dx > 0 && walls_[pointToHorizontalWallIndex(ox, oy)] & HORIZONTAL_WALL)
+            {
+                const int tx = x + 3 - dy;
+                const int ty = y + dx - 1;
+
+                tmpWall[pointToVerticalWallIndex(tx, ty)] |= VERTICAL_WALL;
+            }
+            if (dy > 0 && walls_[pointToVerticalWallIndex(ox, oy)] & VERTICAL_WALL)
+            {
+                const int tx = x + 4 - dy;
+                const int ty = y + dx - 1;
+
+                tmpWall[pointToHorizontalWallIndex(tx, ty)] |= HORIZONTAL_WALL;
+            }
+        }
+    }
+
+    walls_ = std::move(tmpWall);
+
+    clearBoundary();
+}
+
+bool WallBoard::CheckReachability(Point playerPos, int target) const
+{
+    return false;
+}
+
+std::string WallBoard::ToString() const
+{
+    std::stringstream ss;
+
+    // build top line
+    for (int x = 0; x < BOARD_SIZE + 1; ++x)
+    {
+        if (x == 0)
+        {
+            ss << "┌";
+        }
+        else if (x == BOARD_SIZE)
+        {
+            ss << "───┐";
+        }
+        else
+        {
+            ss << "───┬";
+        }
+    }
+    ss << "\n";
+
+    // build middle
+    for (int y = 1; y <= BOARD_SIZE; ++y)
+    {
+        // build players or vertical lines
+        for (int x = 0; x < BOARD_SIZE + 1; ++x)
+        {
+            if (x == 0)
+            {
+                ss << "│";
+            } 
+            else
+            {
+                ss << "   ";
+
+                if (IsVerticalWallPlaced(x, y))
+                    ss << "┃";
+                else
+                    ss << "│";
+            }
+        }
+        ss << "\n";
+
+        // build horizontal lines
+        if (y <= BOARD_SIZE - 1)
+        {
+            for (int x = 0; x < BOARD_SIZE + 1; ++x)
+            {
+                if (x == 0)
+                {
+                    ss << "├";
+                } 
+                else
+                {
+                    if (IsHorizontalWallPlaced(x, y))
+                        ss << "━━━";
+                    else
+                        ss << "───";
+
+                    if (x == BOARD_SIZE)
+                    {
+                        ss << "┤";
+                    }
+                    else
+                    {
+                        ss << "┼";
+                    }
+                }
+            }
+            ss << "\n";
+        }
+    }
+
+    // build bottom line
+    for (int x = 0; x < BOARD_SIZE + 1; ++x)
+    {
+        if (x == 0)
+        {
+            ss << "└";
+        }
+        else if (x == BOARD_SIZE)
+        {
+            ss << "───┘";
+        }
+        else
+        {
+            ss << "───┴";
+        }
+    }
+    ss << "\n";
+
+
+    return ss.str();
+}
+
+void WallBoard::clearBoundary()
+{
+    for (int i = 0; i <= BOARD_SIZE + 1; ++i)
+    {
+        walls_[pointToHorizontalWallIndex(i, 0)] &= ~HORIZONTAL_WALL;
+        walls_[pointToHorizontalWallIndex(i, BOARD_SIZE)] &= ~HORIZONTAL_WALL;
+
+        walls_[pointToVerticalWallIndex(0, i)] &= ~VERTICAL_WALL;
+        walls_[pointToVerticalWallIndex(BOARD_SIZE, i)] &= ~VERTICAL_WALL;
+    }
+
+    for (int i = 0; i <= BOARD_SIZE; ++i)
+    {
+        walls_[pointToHorizontalWallIndex(0, i)] &= ~HORIZONTAL_WALL;
+        walls_[pointToHorizontalWallIndex(BOARD_SIZE + 1, i)] &=
+            ~HORIZONTAL_WALL;
+
+        walls_[pointToVerticalWallIndex(i, 0)] &= ~VERTICAL_WALL;
+        walls_[pointToVerticalWallIndex(i, BOARD_SIZE + 1)] &= ~VERTICAL_WALL;
+    }
+}
+
 Game::Game()
 {
-    SetPlayerPosition(Player::BLUE, Point((1 + BOARD_SIZE) / 2, BOARD_SIZE));
-    SetPlayerPosition(Player::RED, Point((1 + BOARD_SIZE) / 2, 1));
+    setPlayerPosition(Player::BLUE, Point((1 + BOARD_SIZE) / 2, BOARD_SIZE));
+    setPlayerPosition(Player::RED, Point((1 + BOARD_SIZE) / 2, 1));
 }
 
 Player Game::GetCurrentPlayer() const
@@ -25,22 +211,234 @@ int Game::GetRemainWallCount(Player player) const
     return remainWallCounts_[static_cast<int>(player)];
 }
 
-void Game::SetPlayerPosition(Player player, Point newPosition)
-{
-    playerPositions_[static_cast<int>(player)] = newPosition;
-}
-
 Point Game::GetPlayerPosition(Player player) const
 {
     return playerPositions_[static_cast<int>(player)];
+}
+
+bool Game::IsHorizontalWallPlaced(Point pos) const
+{
+    return wallBoard_.IsHorizontalWallPlaced(pos.X(), pos.Y());
+}
+
+bool WallBoard::IsHorizontalWallPlaced(int x, int y) const
+{
+    return walls_[pointToHorizontalWallIndex(x, y)] & HORIZONTAL_WALL;
+}
+
+bool Game::IsVerticalWallPlaced(Point pos) const
+{
+    return wallBoard_.IsVerticalWallPlaced(pos.X(), pos.Y());
+}
+
+bool WallBoard::IsVerticalWallPlaced(int x, int y) const
+{
+    return walls_[pointToVerticalWallIndex(x, y)] & VERTICAL_WALL;
+}
+
+Player Game::GetWinnter() const
+{
+    if (GetPlayerPosition(Player::BLUE).Y() == 1)
+        return Player::BLUE;
+
+    if (GetPlayerPosition(Player::RED).Y() == BOARD_SIZE)
+        return Player::RED;
+
+    // not finished
+    return Player::NONE;
 }
 
 std::string Game::ToString() const
 {
     std::stringstream ss;
 
-    
+    // build top line
+    for (int x = 0; x < BOARD_SIZE + 1; ++x)
+    {
+        if (x == 0)
+        {
+            ss << "┌";
+        }
+        else if (x == BOARD_SIZE)
+        {
+            ss << "───┐";
+        }
+        else
+        {
+            ss << "───┬";
+        }
+    }
+    ss << "\n";
+
+    for (int y = 0; y <= BOARD_SIZE + 1; ++y)
+    {
+        for (int x = 0; x <= BOARD_SIZE + 1; ++x)
+        {
+        }
+    }
 
     return ss.str();
+}
+
+bool Game::IsValidAction(const Action& action, Player player) const
+{
+    if (player == Player::NONE)
+    {
+        player = player_;
+    }
+
+    switch (action.GetType())
+    {
+        case ActionType::MOVE:
+            return isValidMove(static_cast<const Actions::Move&>(action),
+                               player);
+        case ActionType::PLACE_HORIZONTAL_WALL:
+            return isValidPlaceHorizontalWall(
+                static_cast<const Actions::PlaceHorizontalWall&>(action),
+                player);
+        case ActionType::PLACE_VERTICAL_WALL:
+            return isValidPlaceVerticalWall(
+                static_cast<const Actions::PlaceVerticalWall&>(action), player);
+        case ActionType::ROTATE:
+            return isValidRotate(static_cast<const Actions::Rotate&>(action),
+                                 player);
+    }
+
+    throw std::invalid_argument("invalid action type");
+}
+
+void Game::Play(const Action& action, Player player)
+{
+}
+
+bool Game::isValidMove(const Actions::Move& action, Player player) const
+{
+    const Point currentPos = GetPlayerPosition(player);
+    Point newPos = currentPos;
+
+    if (action.GetDirection() == Actions::Move::Direction::UP)
+    {
+        if (wallBoard_.IsHorizontalWallPlaced(currentPos.X(),
+                                              currentPos.Y() - 1))
+        {
+            return false;
+        }
+
+        newPos.Y() += 1;
+    }
+    else if (action.GetDirection() == Actions::Move::Direction::DOWN)
+    {
+        if (wallBoard_.IsHorizontalWallPlaced(currentPos.X(), currentPos.Y()))
+        {
+            return false;
+        }
+
+        newPos.Y() -= 1;
+    }
+    else if (action.GetDirection() == Actions::Move::Direction::LEFT)
+    {
+        if (wallBoard_.IsVerticalWallPlaced(currentPos.X() - 1, currentPos.Y()))
+        {
+            return false;
+        }
+
+        newPos.X() -= 1;
+    }
+    else if (action.GetDirection() == Actions::Move::Direction::RIGHT)
+    {
+        if (wallBoard_.IsVerticalWallPlaced(currentPos.X(), currentPos.Y()))
+        {
+            return false;
+        }
+
+        newPos.X() += 1;
+    }
+
+    const bool inBoard = (newPos.X() > 0 && newPos.X() <= BOARD_SIZE) &&
+                         (newPos.Y() > 0 && newPos.X() <= BOARD_SIZE);
+
+    return inBoard;
+}
+
+bool Game::isValidPlaceHorizontalWall(
+    const Actions::PlaceHorizontalWall& action,
+    [[maybe_unused]] Player player) const
+{
+    const Point pos = action.GetPosition();
+
+    const bool inBoard = (pos.X() > 0 && pos.X() <= WALL_PLACEABLE_SIZE) &&
+                         (pos.Y() > 0 && pos.Y() <= WALL_PLACEABLE_SIZE);
+    if (!inBoard)
+        return false;
+
+    if (wallBoard_.IsVerticalWallPlaced(pos.X(), pos.Y()) &&
+        wallBoard_.IsVerticalWallPlaced(pos.X(), pos.Y() + 1))
+        return false;
+
+    if (wallBoard_.IsHorizontalWallPlaced(pos.X(), pos.Y()) ||
+        wallBoard_.IsHorizontalWallPlaced(pos.X() + 1, pos.Y()))
+        return false;
+
+    WallBoard tmpBoard{ wallBoard_ };
+    tmpBoard.PlaceHorizontalWall(pos.X(), pos.Y());
+
+    if (!tmpBoard.CheckReachability(GetPlayerPosition(Player::BLUE), 1) ||
+        !tmpBoard.CheckReachability(GetPlayerPosition(Player::RED), BOARD_SIZE))
+        return false;
+
+    return true;
+}
+
+bool Game::isValidPlaceVerticalWall(const Actions::PlaceVerticalWall& action,
+                                    [[maybe_unused]] Player player) const
+{
+    const Point pos = action.GetPosition();
+
+    const bool inBoard = (pos.X() > 0 && pos.X() <= WALL_PLACEABLE_SIZE) &&
+                         (pos.Y() > 0 && pos.Y() <= WALL_PLACEABLE_SIZE);
+    if (!inBoard)
+        return false;
+
+    if (wallBoard_.IsVerticalWallPlaced(pos.X(), pos.Y()) ||
+        wallBoard_.IsVerticalWallPlaced(pos.X(), pos.Y() + 1))
+        return false;
+
+    if (wallBoard_.IsHorizontalWallPlaced(pos.X(), pos.Y()) &&
+        wallBoard_.IsHorizontalWallPlaced(pos.X() + 1, pos.Y()))
+        return false;
+
+    WallBoard tmpBoard{ wallBoard_ };
+    tmpBoard.PlaceVerticalWall(pos.X(), pos.Y());
+
+    if (!tmpBoard.CheckReachability(GetPlayerPosition(Player::BLUE), 1) ||
+        !tmpBoard.CheckReachability(GetPlayerPosition(Player::RED), BOARD_SIZE))
+        return false;
+
+    return true;
+}
+
+bool Game::isValidRotate(const Actions::Rotate& action,
+                         [[maybe_unused]] Player player) const
+{
+    const Point pos = action.GetPosition();
+
+    const bool inBoard = (pos.X() > 0 && pos.X() <= ROTATABLE_SIZE) &&
+                         (pos.Y() > 0 && pos.Y() <= ROTATABLE_SIZE);
+    if (!inBoard)
+        return false;
+
+    WallBoard tmpBoard{ wallBoard_ };
+    tmpBoard.Rotate(pos.X(), pos.Y());
+
+    if (!tmpBoard.CheckReachability(GetPlayerPosition(Player::BLUE), 1) ||
+        !tmpBoard.CheckReachability(GetPlayerPosition(Player::RED), BOARD_SIZE))
+        return false;
+
+    return true;
+}
+
+void Game::setPlayerPosition(Player player, Point newPosition)
+{
+    playerPositions_[static_cast<int>(player)] = newPosition;
 }
 }  // namespace fights
